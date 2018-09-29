df615d7e0aab1d3d214014611a6df2fa9daf3f80
mario <mandresbrubio@gmail.com>
2018-09-29 14:38:35 -0500

    bug fixes

--- server/src/app.js
+++ server/src/app.js
@@ -9,10 +9,10 @@ const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
-app.use('/', serveStatic(path.join(__dirname, '../client/dist')))
+app.use(express.static(path.join(__dirname, '../client/dist')))
 
 app.get('/posts', (req, res) => {
   res.send([{time: 60, payout: Math.floor(Math.random() * (5 - 1 + 1)) + 2}, {time: 120, payout: Math.floor(Math.random() * (8 - 1 + 1)) + 5}])
 })
 
-app.listen(process.env.PORT || 8081)
+app.listen(process.env.PORT || 5000)
